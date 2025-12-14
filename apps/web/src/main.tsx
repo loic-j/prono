6d6bbac7c7bd344add00245ea6985b2c7c1fdd1b
loic-j <loic.jacquel@gmail.com>
2025-12-14 17:35:28 +0900

    fix route with supertokens

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SuperTokensWrapper } from "supertokens-auth-react";
 import { SessionAuth } from "supertokens-auth-react/recipe/session";
-import { getRoutingComponent, canHandleRoute } from "supertokens-auth-react/ui";
+import {
+  getRoutingComponent,
+  canHandleRoute,
+  getSuperTokensRoutesForReactRouterDom,
+} from "supertokens-auth-react/ui";
 import { EmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/emailpassword/prebuiltui";
-import App from "./App";
 import "./index.css";
 import "./supertokens.config";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import * as reactRouterDom from "react-router-dom";
 
 function AppWithAuth() {
   // If SuperTokens can handle this route (e.g., /auth), show its UI
@@ -19,7 +26,15 @@ function AppWithAuth() {
   return (
     <SuperTokensWrapper>
       <BrowserRouter>
-        <App />
+        <Routes>
+          {/*This renders the login UI on the /auth route*/}
+          {getSuperTokensRoutesForReactRouterDom(reactRouterDom, [
+            EmailPasswordPreBuiltUI,
+          ])}
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </BrowserRouter>
     </SuperTokensWrapper>
   );
